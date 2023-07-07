bbd65d9c1ecf542e2e3199fce6ef2075c92b05d1
EliottDup <eliott.duparc@gmail.com>
2023-07-07 21:27:49 +0200

    added getTileFloor()

--- Room.js
+++ Room.js
@@ -1,13 +1,19 @@
 class Room {
     constructor(Room, tileSize, tiles) {
-        this.pattern = Room[map];
+        this.map = Room[map];
         this.tileSize = tileSize;
         this.height = roomPattern.length;
         this.width = roomPattern[0].length;
         this.tiles = tiles;
     }
 
+    getTileFloor(x, y){
+        tileX = Math.floor(x / this.tileSize);
+        tileY = Math.floor(y / this.tileSize);
+        return map[tileY][tileX];
+    }
+
     draw() {
-        noSmooth
+        noSmooth();
     }
 }
