69715ae89c79fc6ac1b5593de4697fdab0bf5126
EliottDup <eliott.duparc@gmail.com>
2023-07-08 09:39:25 +0200

    room interact almost working

--- Room.js
+++ Room.js
@@ -10,7 +10,7 @@ class Room {
     interact(x, y) {
         let tileX = Math.floor(x / this.tileSize);
         let tileY = Math.floor(y / this.tileSize);
-
+        console.log(tileX, tileY);
         for (x = -1; x < 2; x++) {
             for (y = -1; y < 2; y++) {
                 for (let i = 0; i < this.levers.length; i++) {
@@ -24,8 +24,8 @@ class Room {
                         this.levers[i][0] == tileX + x &&
                         this.levers[i][1] == tileY + y
                     ) {
-                        map[this.levers[i][3]][this.levers[i][2]] =
-                            !map[this.levers[i][3]][this.levers[i][2]];
+                        this.map[this.levers[i][3]][this.levers[i][2]] =
+                            !this.map[this.levers[i][3]][this.levers[i][2]];
                     }
                 }
             }
